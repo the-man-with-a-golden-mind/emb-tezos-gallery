ef85441788ee2ff729869fe18af41844116c826d
Michał <michmajchrzak@gmail.com>
2022-08-04 17:59:14 +0200

    clear run.scm

--- run.scm
+++ run.scm
@@ -5,12 +5,6 @@
 
 (import (bot gallery))
 
-(define (run)
-  (let* ((tokens (get-tokens))
-         (files (get-files tokens)))
-    (print tokens))
-  )
-
 (run-tezos-downloader-actor)
 (mail 'nft-tezos-downloader ['start "tz2JPfBB2fpf9DRzXjVi5U4CAEoENU2dnz8e" 5000])
 
